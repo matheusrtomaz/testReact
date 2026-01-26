0cf6dc71b1dc5f10d8e49b11685f9a2face38bce
Matheus Rodrigues Tomaz <157989074+matheusrtomaz@users.noreply.github.com>
2026-01-26 14:46:07 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Props from './Props'
 
 function App() {
 
   return (
     <div>
       <h1>Testando React</h1>
+      <Props nome="Matheus" idade="28" />
+      <Props nome="Elaine" idade="45" />
     </div>
   )
 }
---
+++ src/Props.jsx
@@ -0,0 +1,10 @@
+function Props(props) {
+    return (
+        <div>
+            <h2>Nome :{props.nome}</h2>
+            <h3>Idade :{props.idade}</h3>
+        </div>
+    )
+}
+
+export default Props
